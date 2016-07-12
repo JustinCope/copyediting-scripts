7b9524e43202da3b1d3644ae9e8b4dfe4e05eec8
Justin Cope <justin.cope@utexas.edu>
2016-07-12 09:46:59 -0500

    updated to run as a script with args from terminal

--- find.math.exp.py
+++ find.math.exp.py
@@ -1,6 +1,5 @@
 import re
-
-
+import sys
 
 def getResults(data, regex):
     exps = []
@@ -9,10 +8,6 @@ def getResults(data, regex):
         for match in matches:
             exps.append(match)
     return exps
-
-# exps.remove('$_$')
-
-# define a frequency function here
 
 def getFrequencies(exps):
     dict = {}
@@ -23,61 +18,30 @@ def getFrequencies(exps):
             dict[n] += 1
     return dict
 
-
-# 	for each n in exps 
-#      if n not key of dict
-#         dict[n] = 1
-#      else
-#         dict[n] += 1
-
-
 ###########################################
 
-sourceName = 'kamp-2016.v6.tex'
+source = sys.argv[1] # LaTeX file to search
+target = sys.argv[2] # Name for the output file
 
-source = open(sourceName, 'r')
+# Check for custom regex from terminal input, otherwise default to math delimiter search
+if length(sys.argv) == 4:
+	regex = sys.argv[3]
+else:
+	regex = '\$.+?\$'
 
-targetName = 'math.output.tex'
+regexComplete = r'{0}'.format(regex)
 
+search = re.compile(regexComplete)
+	
+source = open(source, 'r')
 
-###########################################
+output = getFrequencies(getResults(source,search))
 
-searchA = re.compile(r'\$.+?\$')
-searchB = re.compile(r'\\.+?_\\model')
-searchC = re.compile(r'\$[^\$]+?_[^\$]+?\$')
+g = open(target, 'w')
 
-search = searchA
+g.write("\\documentclass{article} \n \\input{frontmatter} \n \\begin{document} \n\n")
 
-###########################################
-
-
-#output = sorted(list(set(getResults(source,search))))
-output = sorted(getResults(source,search))
-output2 = getFrequencies(output)
-
-g = open(targetName, 'w')
-
-g.write("\\documentclass[lucida]{sp} \n\n \\title{} \n \\author{\\spauthor{}} \n\n")
-
-g.write("\\usepackage{drs} \n") 
-g.write("\\usepackage{example} \n") 
-g.write("\\usepackage{makecell} \n") 
-g.write("\\usepackage{booktabs} \n") 
-g.write("\\usepackage{moreenum} \n") 
-g.write("\\usepackage{mathrsfs} \n \n") 
-
-g.write("\\DeclareMathAlphabet{\\mathpzc}{OT1}{pzc}{m}{it} %\ Defines \mathpzc{} \n \n")
-
-g.write("\\input{macros} \n")
-g.write("\\input{drsrevised} \n\n")
-
-g.write("\\begin{document} \n\n")
-
-# for x in output:
-# 	g.write(x)
-# 	g.write("\n\n")
-
-for w in sorted(output2, key=output2.get, reverse=True):
+for w in sorted(output, key=output.get, reverse=True):
 	g.write(w + "\hspace{\\fill}" + str(output2[w]) + "\n\n")
 
 g.write("\\end{document}\n")
